1a48b2c788c7cee32a753981da8a8d4845bcf7b5
Penahli Ibrahim <penahliibrahim58@gmail.com>
2023-09-17 17:07:33 +0400

    C# Delegate

---
+++ Delegate/MyCLass.cs
@@ -0,0 +1,25 @@
+﻿namespace Delegate;
+
+public class MyCLass
+{
+    private string inputString;
+
+    public MyCLass(string inputString)
+    {
+        this.inputString = inputString;
+    }
+
+    public void Space(string str)
+    {
+        string spacedStr = string.Join("_", str.ToCharArray());
+        Console.WriteLine(spacedStr);
+    }
+
+    public void Reverse(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedStr = new string(charArray);
+        Console.WriteLine(reversedStr);
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using Delegate;
+
+public delegate void Func(string str);
+
+public class Run
+{
+    public void runFunc(Func func, string str)
+    {
+        func.Invoke(str);
+    }
+}
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.Write("Enter string: ");
+        var str = Console.ReadLine();
+        MyCLass cls = new MyCLass(str);
+
+        Func funcDell = new Func(cls.Space);
+        Func funcDell2 = new Func(cls.Reverse);
+
+        Run run = new Run();
+        run.runFunc(funcDell, str);
+        run.runFunc(funcDell2, str);
+    }
+}
